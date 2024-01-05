44632e74f8c49610f3ecc3802148e1d8bc394fce
Jacoby Lockman <jlockman@g.hmc.edu>
2024-01-05 14:48:23 -0700

    bugs fixed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,21 @@
 class PostsController < ApplicationController
   def new
     @post = Post.new
+    @like = current_user.likes.where(post_id: @post.id)
+
   end
 
   def show
     @post = Post.find(params[:id])
+    @like = current_user.likes.where(post_id: @post.id)
+
   end
 
   def create
     @post = Post.new(post_params)
     @post.user_id = current_user.id
+    @like = current_user.likes.where(post_id: @post.id)
+
 
     if @post.valid?
       @post.save
